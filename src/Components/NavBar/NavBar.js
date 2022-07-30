54804c2ee925c95542c4f051556106c424da096b
Camila Salvi <100172223+CamilaSalvi@users.noreply.github.com>
2022-07-30 16:45:01 -0300

    Add files via upload

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,25 @@
 import './NavBar.css';
+import corazon from './corazon.png';
+import hilo from './hilo.png';
 
 function NavBar() {
   return (
-    <div>
-      <ul>
-        <li>
+    <div> 
+      <h1 className='titulo'>Lady Threads</h1>
+      <ul className='nabbar'>
+        <img src={hilo} className='hilo'></img>
+
+        <li className='nabbar__items'>
           Mi tienda
         </li>
-        <li>
+        <li className='nabbar__items'>
           Artículos 
         </li>
-        <li>
+        <li className='nabbar__items'>
           Contacto 
         </li>
-        <li> Medios de Pago</li>
+        <li className='nabbar__items'> Medios de Pago</li>
+        <img src={corazon} alt='corazon rojo fondo transparente' className='corazon'></img>
       </ul>
     </div>
   );
