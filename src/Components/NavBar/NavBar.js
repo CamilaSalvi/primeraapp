261d301878e1473f193a29fa9c280f655e79190b
Camila Salvi <100172223+CamilaSalvi@users.noreply.github.com>
2022-07-30 15:32:59 -0300

    Add files via upload

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>
+          Mi tienda
+        </li>
+        <li>
+          Artículos 
+        </li>
+        <li>
+          Contacto 
+        </li>
+        <li> Medios de Pago</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
